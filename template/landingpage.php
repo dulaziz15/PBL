23273c4b20e4a85826bebc23977aba434479f0d8
NaufalPratomo <naufalpratomo8@gmail.com>
2024-12-03 09:06:48 +0700

    template landing page

--- template/landingpage.php
+++ template/landingpage.php
@@ -1,57 +1,59 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="style.css" />
-  <link rel="stylesheet" href="landingPage.css" />
-  <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Landing Page</title>
+  <link rel="stylesheet" href="landingpage.css">
 </head>
 
 <body>
+  <!-- Navbar -->
+  <nav class="navbar">
+    <img src="logo.png" alt="Logo" class="logo">
+    <a href="#home">Home</a>
+    <a href="#alur">Alur Bebas Tanggungan</a>
+    <a href="#kontak">Kontak</a>
+  </nav>
 
-  <img class="polygon-2" src="images/polygon-20.svg" />
-  <img class="polygon-4" src="images/polygon-40.svg" />
-  <img class="polygon-6" src="images/polygon-60.svg" />
-  <img class="polygon-3" src="images/polygon-30.svg" />
-  <img class="polygon-5" src="images/polygon-50.svg" />
-  <img class="vector-1" src="images/vector-10.svg" />
-  <div class="sign-in">Sign In</div>
-  <div class="upload-dokumen">
-    Upload
-    <br />
-    Dokumen
-  </div>
-  <div class="verifikasi">Verifikasi</div>
-  <div class="cetak-bebas-tanggungan">
-    Cetak
-    <br />
-    Bebas Tanggungan
-  </div>
-  <div class="request-bebas-tanggungan">
-    Request
-    <br />
-    Bebas Tanggungan
-  </div>
-  <div class="ellipse-3"></div>
-  <div class="ellipse-5"></div>
-  <div class="ellipse-6"></div>
-  <div class="ellipse-7"></div>
-  <div class="ellipse-8"></div>
-  <div class="ellipse-9"></div>
-  <div class="ellipse-4"></div>
-  <div class="_01">01</div>
-  <div class="_02">02</div>
-  <div class="_03">03</div>
-  <div class="_04">04</div>
-  <div class="_05">05</div>
-  <img class="circle-user-solid-1" src="images/circle-user-solid-10.svg" />
-  <img class="file-zipper-solid-1" src="images/file-zipper-solid-10.svg" />
-  <img class="circle-check-solid-3" src="images/circle-check-solid-30.svg" />
-  <img class="file-solid-2" src="images/file-solid-20.svg" />
-  <img class="print-solid-1" src="images/print-solid-10.svg" />
+  <!-- Sections -->
+  <section id="home">
+    </section>
+
+  <section id="alur">
+    </section>
+
+  <section id="kontak">
+    </section>
+
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="footer-section">
+        <h3>Info</h3>
+        <ul>
+          <li><a href="#">Polinema</a></li>
+          <li><a href="#">Webmail</a></li>
+          <li><a href="#">SiAkad</a></li>
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h3>Contact Us</h3>
+        <ul>
+          <li>JL. Soekarno Hatta No. 9 Malang</li>
+          <li>Phone: (0341) 404424, 404425</li>
+          <li>E-mail: Imspolinema@polinema.ac.id</li>
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h3>Get Social</h3>
+        <ul>
+          </ul>
+      </div>
+    </div>
+    <p class="copyright">&copy; 2023 Landing Page Polinema. All rights reserved.</p>
+  </footer>
 </body>
 
 </html>
