cc5c55bac2904cac5f55845f7d7a6707ed36d1a4
NaufalPratomo <naufalpratomo8@gmail.com>
2024-12-04 19:04:00 +0700

    skip

--- Config/koneksi.php
+++ Config/koneksi.php
@@ -2,10 +2,10 @@
 namespace Pbl\Config;
 use PDO;
 
-class koneksi {
+class Koneksi {
 
     public function KoneksiDB() {
-        $conn = new PDO("sqlsrv:server=LAPTOP-60DUFOCJ\SQLEXPRESS;database=BebasTanggunganTADB");
+        $conn = new PDO("sqlsrv:server=LAPTOP-60DUFOCJ\SQLEXPRESS;database=BebasTanggunganDB");
         return $conn;
     }
 
